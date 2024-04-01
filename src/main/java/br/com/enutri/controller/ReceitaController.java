d92527686bfaeb72fc7363e8b20291f5014ebf11
Raphael <raphaelvictorsouza@hotmail.com>
2024-04-01 16:03:39 -0300

    Metodo getReceitaById

--- src/main/java/br/com/enutri/controller/ReceitaController.java
+++ src/main/java/br/com/enutri/controller/ReceitaController.java
@@ -18,23 +18,24 @@ public class ReceitaController {
     @Autowired
     private ReceitaService receitaService;
 
-    @GetMapping(path="/todos")
-    public ResponseEntity<List<ReceitaDTO>> retriveAllReceitas(){
+    @GetMapping(path = "/todos")
+    public ResponseEntity<List<ReceitaDTO>> retriveAllReceitas() {
         List<ReceitaDTO> receitas = receitaService.retriveAllReceitas();
         return new ResponseEntity<>(receitas, HttpStatus.OK);
     }
 
-    @PostMapping(path="/novo")
-    private ResponseEntity<ReceitaDTO> addReceita(@RequestBody ReceitaDTO receitaDTO){
+    @PostMapping(path = "/novo")
+    private ResponseEntity<ReceitaDTO> addReceita(@RequestBody ReceitaDTO receitaDTO) {
         Receita novaReceita = receitaService.save(receitaDTO);
         ReceitaDTO savedReceitaDTO = new ReceitaDTO(novaReceita);
 
         return new ResponseEntity<>(savedReceitaDTO, HttpStatus.CREATED);
     }
 
-    @PatchMapping(path="/atualizar/{id}")
-    public ResponseEntity<ReceitaDTO> atualizarReceita(@PathVariable("id") Long id, @RequestBody ReceitaDTO receitaDTO) {
-        if(!receitaService.isExists(id)){
+    @PatchMapping(path = "/atualizar/{id}")
+    public ResponseEntity<ReceitaDTO> atualizarReceita(@PathVariable("id") Long id,
+            @RequestBody ReceitaDTO receitaDTO) {
+        if (!receitaService.isExists(id)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
@@ -43,9 +44,17 @@ public class ReceitaController {
         return new ResponseEntity<>(receitaAtualizadaDTO, HttpStatus.OK);
     }
 
-    @DeleteMapping(path="/deletar/{id}")
-    public ResponseEntity<String> deleteReceita(@PathVariable("id") Long id){
+    @DeleteMapping(path = "/deletar/{id}")
+    public ResponseEntity<String> deleteReceita(@PathVariable("id") Long id) {
         receitaService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ReceitaDTO> getReceita(@PathVariable Long id) {
+        Receita receita = receitaService.getReceitaById(id);
+        ReceitaDTO receitaDTO = new ReceitaDTO(receita);
+        return new ResponseEntity<>(receitaDTO, HttpStatus.OK);
+    }
+
 }
